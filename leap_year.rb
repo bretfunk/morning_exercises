a136cce4a72e42ea1d659e11e228a9990e3d9564
BFunk <bret.e.funk@gmail.com>
2017-05-08 09:31:07 -0600

    first commit

---
+++ leap_year.rb
@@ -0,0 +1,16 @@
+current_year = 2017
+next_twenty = []
+
+while next_twenty.length < 20
+  if current_year % 4 == 0
+      if current_year % 100 != 0
+        next_twenty.push(current_year)
+        current_year += 1
+      else
+        current_year += 1
+      end
+  else
+    current_year += 1
+  end
+end
+puts next_twenty
